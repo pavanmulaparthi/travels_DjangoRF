c6340c3a86e630813f0e6d67554d962165eb979e
pavanmulaparthi <105601103+pavanmulaparthi@users.noreply.github.com>
2025-09-19 21:40:13 +0530

    Add files via upload

---
+++ admin.py
@@ -0,0 +1,19 @@
+
+
+# Register your models here.
+from django.contrib import admin
+from .models import Bus, Seat, Booking
+
+
+class BusAdmin(admin.ModelAdmin):
+    list_display = ('bus_name', 'number', 'origin', 'destination', 'start_time', 'reach_time', 'no_of_seats', 'price')
+
+class SeatAdmin(admin.ModelAdmin):
+    list_display = ('seat_number', 'bus', 'is_booked')
+
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('user', 'bus', 'seat', 'booking_time')
+
+admin.site.register(Bus, BusAdmin)
+admin.site.register(Seat, SeatAdmin)
+admin.site.register(Booking, BookingAdmin)
